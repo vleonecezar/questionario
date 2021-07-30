86cacf4a7654f5a214ef723bdfb00f5887b0256d
Vitor Leone Cezar <vleone.social@gmail.com>
2021-07-30 18:08:56 -0300

    criado array com as questões e suas informações  e um estado para respostas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import React from "react";
 
+const perguntas = [
+  {
+    pergunta: "Qual método é utilizado para criar componentes?",
+    options: [
+      "React.makeComponent()",
+      "React.createComponent()",
+      "React.createElement()",
+    ],
+    resposta: "React.createElement()",
+    id: "p1",
+  },
+  {
+    pergunta: "Como importamos um componente externo?",
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component from "./Component"',
+    id: "p2",
+  },
+  {
+    pergunta: "Qual hook não é nativo?",
+    options: ["useEffect()", "useFetch()", "useCallback()"],
+    resposta: "useFetch()",
+    id: "p3",
+  },
+  {
+    pergunta: "Qual palavra deve ser utilizada para criarmos um hook?",
+    options: ["set", "get", "use"],
+    resposta: "use",
+    id: "p4",
+  },
+];
+
 const App = () => {
-  return <div></div>;
+  const [respostas, setRespostas] = React.useState(
+    perguntas.reduce((acc, item) => {
+      return { ...acc, [item.id]: "" };
+    }, {})
+  );
+
+  return <form></form>;
 };
 
 export default App;
